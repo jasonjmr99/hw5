e430f5b84d80fe1ef51af55153646e687281083d
jasonjmr99 <43255946+jasonjmr99@users.noreply.github.com>
2018-10-12 22:28:14 -0700

    Create lewitt-584H.js

---
+++ lewitt-584H.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+    background(155);
+ 
+  
+  noStroke();  
+  fill(255, 40, 0)
+  rect (200, 200, 200,200)
+  
+  fill(75,150,40);
+  rect(200, 0, 200, 200)
+  
+  fill(150,0,170);
+  rect(0, 0 , 200, 200);
+
+    
+}
