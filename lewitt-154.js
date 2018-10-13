2c77e92af35106a2e713fe5f43b98d7b5945ca52
jasonjmr99 <43255946+jasonjmr99@users.noreply.github.com>
2018-10-12 22:11:37 -0700

    Create lewitt-154.js

---
+++ lewitt-154.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(255);
+
+  colorMode(RGB);
+
+  noFill();
+  stroke(0);
+  rect(0, 0, 399, 399);
+
+  
+  stroke(255,0 ,0);
+  line(250, 200, 0, 200)
+
+
+}
